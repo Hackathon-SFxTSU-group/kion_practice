ab85dcc3ac161e6c444429fc9416b824ad6a0e20
Krasti <krasnov.d.a.20@mail.ru>
2025-07-25 00:31:21 +0300

    fix app for future docker

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
 # Импорт необходимых модулей
 import os
-import torch
 import pickle
-
-from utils.video_tools import save_scenes_ffmpeg
-from classes.audio_analyzer import AudioSceneAnalyzer
-from classes.video_analyzer_complex import VideoPipeline
+#from classes.video_analyzer_complex import VideoPipeline
+from classes.video_anlyzer_without_faceraid import VideoPipeline
 from classes.scene_enricher import SceneEnricher
-from classes.simple_transcriptor import ASRProcessor
 from classes.openai import OpenAIThemer
+from classes.simple_transcriptor import ASRProcessor
 from classes.scene_merge import UnifiedSceneMerger
+import matplotlib.pyplot as plt
+from classes.audioanalyzer_and_transcriptor import AudioAnalyzer
 
 
-VIDEO_PATH = "videos/Video_01.mp4"          # Путь к видеофайлу
+VIDEO_PATH = "videos/Video_02.avi"          # Путь к видеофайлу
 AUDIO_PATH = "temp/temp_audio.wav"           # Временный WAV
 OUTPUT_DIR = "output_files/"
 TEMP_DIR = "temp/"         # Папка для результатов
@@ -20,36 +19,39 @@ CACHE_PATH = os.path.join(TEMP_DIR, "results.pkl")
 MIN_SCENE_LENGTH = 2.0                  # Мин. длительность сцены
 MAX_SCENE_LENGTH = 300.0                # Макс. допустимая длина сцены (5 мин)
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(f"Using device: {device}")
-
-
 if os.path.exists(CACHE_PATH):
     print("🔁 Загружаем из кеша...")
     with open(CACHE_PATH, "rb") as f:
-        scenes, track_faces, tracking_frames, track_id_to_person = pickle.load(f)
+        #scenes, track_faces, tracking_frames, track_id_to_person = pickle.load(f)
+        scenes, track_faces, tracking_frames = pickle.load(f)
 
 else:
     pipeline = VideoPipeline()
-    scenes, track_faces, tracking_frames, track_id_to_person = pipeline.run(VIDEO_PATH, OUTPUT_DIR)
+    #scenes, track_faces, tracking_frames, track_id_to_person = pipeline.run(VIDEO_PATH, OUTPUT_DIR)
+    scenes, track_faces, tracking_frames = pipeline.run(VIDEO_PATH, OUTPUT_DIR)
 
     with open(CACHE_PATH, "wb") as f:
-        pickle.dump((scenes, track_faces, tracking_frames, track_id_to_person), f)
+        #pickle.dump((scenes, track_faces, tracking_frames, track_id_to_person), f)
+        pickle.dump((scenes, track_faces, tracking_frames), f)
 
-splitter = AudioSceneAnalyzer(
+# Запуск анализа
+splitter = AudioAnalyzer(
     video_path=VIDEO_PATH,
     output_dir=OUTPUT_DIR,
     whisper_model_size='small'
 )
 splitter.run(sensitivity=0.88, min_scene_duration=2.0)
-segments = splitter.transcribe_audio()
+
+segments_short = splitter.segments_short  # для отчётов и группировок
+segments_full = splitter.segments   
+
+# Вычисление аудиоэнергии по кадрам
 energy = splitter.detect_audio_activity(frame_duration=1.0)
-enricher = SceneEnricher(segments, energy)
-scenes_final = enricher.run(scenes, track_id_to_person)
+enricher = SceneEnricher(segments_full, energy)
+#scenes_final = enricher.run(scenes, track_id_to_person)
+scenes_final = enricher.run(scenes)
 #scenes_final = enricher.run(scene_data, track_id_to_person, print_report=False)
 
-asr = ASRProcessor()
-segments_short = asr.process(VIDEO_PATH)
 themer = OpenAIThemer(
         api_key="",  # Добавь свой ключ
         base_url="https://api.proxyapi.ru/openai/v1"
